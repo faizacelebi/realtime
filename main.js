b911e108c6d6f0d3e4c2c127db6d4674fbdc2702
faizacelebi <84246436+faizacelebi@users.noreply.github.com>
2023-01-18 17:49:06 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+noseX=0;
+noseY=0;
+function setup(){
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+
+    canvas = createCanvas(550, 550);
+    canvas.position(560, 150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX = " + noseX +"+nosey = " + noseY);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWrstX = results[0].pose.rightWrist.x;
+        difference = floor(leftWrist - rightWrist);
+
+        console.log("leftWrist = " + leftWrist + "rightWrist = "+ rightWristX + "difference = " + difference)
+    }
+}
+
+function modelLoaded(){
+    console.log('PoseNet Is Initialized!');
+}
+
+function draw(){
+    background('#969A97');
+
+    document.getElementById("square_side").innerHTML = "Width And Height of a Square will be = " + difference +"px"
+    fill('#F90093');
+    stroke('#F90093');
+    square(noseX, noseY, difference);
+}
